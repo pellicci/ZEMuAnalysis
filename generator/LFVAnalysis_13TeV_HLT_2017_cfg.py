0e6f1a45dda92bf5610f691b58afd2d8b49c56d5
Mario <mpel@protonmail.com>
2020-10-16 09:42:54 +0200

    adding HLT for 2017

---
+++ generator/LFVAnalysis_13TeV_HLT_2017_cfg.py
@@ -0,0 +1,89 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: step1 --filein file:ZEMuAnalysis_pythia8_DIGI_2017.root --fileout file:ZEMuAnalysis_pythia8_HLT_2017.root --mc --eventcontent PREMIXRAW --datatier GEN-SIM-RAW --conditions 94X_mc2017_realistic_v11 --step HLT:2e34v40 --datamix PreMix --era Run2_2017 --python_filename ZEMuAnalysis_13TeV_pythia8_HLT_2017_cfg.py --no_exec -n 10
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.StandardSequences.Eras import eras
+
+process = cms.Process('HLT',eras.Run2_2017)
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('HLTrigger.Configuration.HLT_2e34v40_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10)
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('file:ZEMuAnalysis_pythia8_DIGI_2017.root'),
+    secondaryFileNames = cms.untracked.vstring()
+)
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('step1 nevts:10'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+# Output definition
+
+process.PREMIXRAWoutput = cms.OutputModule("PoolOutputModule",
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('GEN-SIM-RAW'),
+        filterName = cms.untracked.string('')
+    ),
+    fileName = cms.untracked.string('file:ZEMuAnalysis_pythia8_HLT_2017.root'),
+    outputCommands = process.PREMIXRAWEventContent.outputCommands,
+    splitLevel = cms.untracked.int32(0)
+)
+
+# Additional output definition
+
+# Other statements
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '94X_mc2017_realistic_v11', '')
+
+# Path and EndPath definitions
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.PREMIXRAWoutput_step = cms.EndPath(process.PREMIXRAWoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule()
+process.schedule.extend(process.HLTSchedule)
+process.schedule.extend([process.endjob_step,process.PREMIXRAWoutput_step])
+from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
+associatePatAlgosToolsTask(process)
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from HLTrigger.Configuration.customizeHLTforMC
+from HLTrigger.Configuration.customizeHLTforMC import customizeHLTforMC 
+
+#call to customisation function customizeHLTforMC imported from HLTrigger.Configuration.customizeHLTforMC
+process = customizeHLTforMC(process)
+
+# End of customisation functions
+
+# Customisation from command line
+
+# Add early deletion of temporary data products to reduce peak memory need
+from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
+process = customiseEarlyDelete(process)
+# End adding early deletion
---
+++ generator/crab_config/multicrab_Z_HLT_2017.py
@@ -0,0 +1,83 @@
+from CRABAPI.RawCommand import crabCommand
+from WMCore.Configuration import Configuration
+from CRABClient.UserUtilities import config
+config = Configuration()
+
+config.section_('General')
+config.General.transferOutputs = True
+config.General.workArea = 'crab_projects/HLT_2017'
+
+config.section_('JobType')
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'LFVAnalysis_13TeV_HLT_2017_cfg.py'
+config.JobType.allowUndistributedCMSSW = True
+config.JobType.maxMemoryMB = 3000
+
+config.section_('Data')
+config.Data.inputDBS = 'phys03'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 2
+config.Data.publication = True
+
+config.Data.outputDatasetTag = 'LFVAnalysis_HLT_947V1'
+
+config.section_('Site')
+config.Site.storageSite = 'T2_IT_Bari'
+
+if __name__ == '__main__':
+
+    from CRABAPI.RawCommand import crabCommand
+    from CRABClient.ClientExceptions import ClientException
+    from httplib import HTTPException
+    from multiprocessing import Process
+
+    def submit(config):
+        try:
+            crabCommand('submit', config = config)
+        except HTTPException as hte:
+            print "Failed submitting task: %s" % (hte.headers)
+        except ClientException as cle:
+            print "Failed submitting task: %s" % (cle)
+
+
+    config.General.requestName = 'LFVAnalysis_ZEMu_HLT_2017_947V1'
+    config.Data.inputDataset = '/LFVAnalysis_ZEMu_2017_934V1/pellicci-LFVAnalysis_DIGIL1_947V1-bd8af8d70f71c72c06687d181ac6a203/USER'
+
+    p = Process(target=submit, args=(config,))
+    p.start()
+    p.join()
+
+    config.General.requestName = 'LFVAnalysis_ZETau_HLT_2017_947V1'
+    config.Data.inputDataset = '/LFVAnalysis_ZETau_2017_934V1/pellicci-LFVAnalysis_DIGIL1_947V1-bd8af8d70f71c72c06687d181ac6a203/USER'
+
+    p = Process(target=submit, args=(config,))
+    p.start()
+    p.join()
+
+    config.General.requestName = 'LFVAnalysis_ZMuTau_HLT_2017_947V1'
+    config.Data.inputDataset = '/LFVAnalysis_ZMuTau_2017_934V1/pellicci-LFVAnalysis_DIGIL1_947V1-bd8af8d70f71c72c06687d181ac6a203/USER'
+
+    p = Process(target=submit, args=(config,))
+    p.start()
+    p.join()
+
+    config.General.requestName = 'LFVAnalysis_HEMu_HLT_2017_947V1'
+    config.Data.inputDataset = '/LFVAnalysis_HEMu_2017_934V1/pellicci-LFVAnalysis_DIGIL1_947V1-bd8af8d70f71c72c06687d181ac6a203/USER'
+
+    p = Process(target=submit, args=(config,))
+    p.start()
+    p.join()
+
+    config.General.requestName = 'LFVAnalysis_HETau_HLT_2017_947V1'
+    config.Data.inputDataset = '/LFVAnalysis_HETau_2017_934V1/pellicci-LFVAnalysis_DIGIL1_947V1-bd8af8d70f71c72c06687d181ac6a203/USER'
+
+    p = Process(target=submit, args=(config,))
+    p.start()
+    p.join()
+
+    config.General.requestName = 'LFVAnalysis_HMuTau_HLT_2017_947V1'
+    config.Data.inputDataset = '/LFVAnalysis_HMuTau_2017_934V1/pellicci-LFVAnalysis_DIGIL1_947V1-bd8af8d70f71c72c06687d181ac6a203/USER'
+
+    p = Process(target=submit, args=(config,))
+    p.start()
+    p.join()
