592acfdab5836f698c2b71aa934402a5e89d76a6
Mario <tech.y3rvn@simplelogin.co>
2022-08-23 14:29:33 +0200

    new 2016 prod

--- generator/crab_config/crab_ZLL_DIGI_2016.py
+++ generator/crab_config/crab_ZLL_DIGI_2016.py
@@ -3,24 +3,22 @@ config = config()
 
 config.section_('General')
 config.General.transferOutputs = True
-config.General.requestName = 'LFVAnalysis_ZLL_DIGI_2016_10612V1'
-config.General.workArea = 'crab_projects'
+config.General.workArea = 'crab_projects/DIGI2016'
+config.General.requestName = 'LFVAnalysis_ZLL_DIGI_2016_8028V1'
 
 config.section_('JobType')
-config.JobType.psetName = 'ZLLAnalysis-2016-DIGI_cfg.py'
 config.JobType.pluginName = 'Analysis'
-config.JobType.outputFiles = ['ZLLAnalysis-2016-DIGI.root']
-#config.JobType.numCores = 8
-
+config.JobType.outputFiles = ['ZEMuAnalysis_pythia8_DIGIL1HLT_2016.root']
 config.JobType.allowUndistributedCMSSW = True
+config.JobType.psetName = 'LFVAnalysis_13TeV_DIGIL1HLT_2016_cfg.py'
 
 config.section_('Data')
 config.Data.inputDBS = 'phys03'
-config.Data.inputDataset = '/LFVAnalysis_ZLL_2016_1064V3/pellicci-LFVAnalysis_ZLL_SIM_2016_10612V1-386081d6edbedfdd33aec5b36c81165f/USER'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1
 config.Data.publication = True
-config.Data.outputDatasetTag = 'LFVAnalysis_ZLL_DIGI_2016_10612V1'
+config.Data.inputDataset = '/LFVAnalysis_ZLL_2016_8028V1_22prodV1/pellicci-LFVAnalysis_ZLL_GENSIM_2016_8028V1_22prodV1-d4fbf6bb88ea36444a4fe79d15bdc21b/USER'
+config.Data.outputDatasetTag = 'LFVAnalysis_ZLL_DIGI_2016_8028V1'
 
 config.section_('Site')
 config.Site.storageSite = 'T2_IT_Bari'
--- generator/crab_config/crab_ZLL_RECO_2016.py
+++ generator/crab_config/crab_ZLL_RECO_2016.py
@@ -3,24 +3,22 @@ config = config()
 
 config.section_('General')
 config.General.transferOutputs = True
-config.General.requestName = 'LFVAnalysis_ZLL_RECO_2016_10612V1'
-config.General.workArea = 'crab_projects'
+config.General.workArea = 'crab_projects/RECO2016'
+config.General.requestName = 'LFVAnalysis_ZLL_RECO_2016_8028V1'
 
 config.section_('JobType')
-config.JobType.psetName = 'ZLLAnalysis-2016-RECO_cfg.py'
 config.JobType.pluginName = 'Analysis'
-config.JobType.outputFiles = ['ZLLAnalysis-2016-RECO.root']
-#config.JobType.numCores = 8
-
+config.JobType.outputFiles = ['ZEMuAnalysis_pythia8_RECO_2016.root']
 config.JobType.allowUndistributedCMSSW = True
+config.JobType.psetName = 'LFVAnalysis_13TeV_RECO_2016_cfg.py'
 
 config.section_('Data')
 config.Data.inputDBS = 'phys03'
-config.Data.inputDataset = '/LFVAnalysis_ZLL_2016_1064V3/pellicci-LFVAnalysis_ZLL_HLT_2016_8033V1-3ab30fc3f54543258c8ebb8fadbb2996/USER'
 config.Data.splitting = 'FileBased'
-config.Data.unitsPerJob = 1
+config.Data.unitsPerJob = 2
 config.Data.publication = True
-config.Data.outputDatasetTag = 'LFVAnalysis_ZLL_RECO_2016_10612V1'
+config.Data.inputDataset = '/LFVAnalysis_ZLL_2016_8028V1_22prodV1/pellicci-LFVAnalysis_ZLL_DIGI_2016_8028V1-6868a286d3522be326f21d858c30a51e/USER'
+config.Data.outputDatasetTag = 'LFVAnalysis_ZLL_RECO_2016_8028V1'
 
 config.section_('Site')
 config.Site.storageSite = 'T2_IT_Bari'
