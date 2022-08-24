6535f0f4c0ae6ff2b489b441ffb96f680443451e
Mario <tech.y3rvn@simplelogin.co>
2022-08-24 09:25:32 +0200

    ÂChanges to GEN for 2016

--- generator/LFVAnalysis_ZLL_13TeV_GENSIM_2016_cfg.py
+++ generator/LFVAnalysis_ZLL_13TeV_GENSIM_2016_cfg.py
@@ -65,8 +65,8 @@ from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, '80X_mcRun2_asymptotic_2016_TrancheIV_v6', '')
 
 from Configuration.Generator.Pythia8CommonSettings_cfi import *
-from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
-from Configuration.Generator.Pythia8aMCatNLOSettings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
+from Configuration.Generator.PSweightsPythia.PythiaPSweightsSettings_cfi import *
 
 process.generator = cms.EDFilter("Pythia8HadronizerFilter",
     maxEventsToPrint = cms.untracked.int32(1),
@@ -76,26 +76,25 @@ process.generator = cms.EDFilter("Pythia8HadronizerFilter",
     comEnergy = cms.double(13000.),
     PythiaParameters = cms.PSet(
         pythia8CommonSettingsBlock,
-        pythia8CUEP8M1SettingsBlock,
-        pythia8aMCatNLOSettingsBlock,
+        pythia8CP5SettingsBlock,
+        pythia8PSweightsSettingsBlock,
         processParameters = cms.vstring(
             'JetMatching:setMad = off',
             'JetMatching:scheme = 1',
             'JetMatching:merge = on',
             'JetMatching:jetAlgorithm = 2',
-            'JetMatching:etaJetMax = 999.',
+            'JetMatching:etaJetMax = 5.',
             'JetMatching:coneRadius = 1.',
             'JetMatching:slowJetPower = 1',
-            'JetMatching:qCut = 30.', #this is the actual merging scale
-            'JetMatching:doFxFx = on',
-            'JetMatching:qCutME = 10.',#this must match the ptj cut in the lhe generation step
-            'JetMatching:nQmatch = 5', #4 corresponds to 4-flavour scheme (no matching of b-quarks), 5 for 5-flavour scheme
-            'JetMatching:nJetMax = 2', #number of partons in born matrix element for highest multiplicity
-            'TimeShower:mMaxGamma = 4.0'
+            'JetMatching:qCut = 19.', #this is the actual merging scale
+            'JetMatching:nQmatch = 5', #5 for 5-flavour scheme (matching of b-quarks)
+            'JetMatching:nJetMax = 4', #number of partons in born matrix element for highest multiplicity
+            'JetMatching:doShowerKt = off', #off for MLM matching, turn on for shower-kT matching
+            'TimeShower:mMaxGamma = 4.0',
         ),
         parameterSets = cms.vstring('pythia8CommonSettings',
-                                    'pythia8CUEP8M1Settings',
-                                    'pythia8aMCatNLOSettings',
+                                    'pythia8CP5Settings',
+                                    'pythia8PSweightsSettings',
                                     'processParameters',
                                     )
     )
